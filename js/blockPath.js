49a27d58b77dd5e2c090df0d6bd8cad980634e63
Steven de Valk <s1129787@student.windesheim.nl>
2020-01-31 17:33:10 +0100

    remove checkDrawPath from blockPath.js

--- js/blockPath.js
+++ js/blockPath.js
@@ -130,62 +130,6 @@ function displayAlert(text){
     gameWinHeader.innerHTML = text;
 }
 
-// function checkDrawPath(el){
-//     if(game.mouseDown == true && el.innerHTML==""){
-
-//         var x = indexInClass(el)%levels[currentLevel].gridWidth;
-//         var y = indexInClass(el.parentElement)
-//         var index = indexInClass(el)
-
-//         game.path.push(index)
-//         game.coordinates.push([x, y, index]);
-        
-//         previousCoordinates = game.coordinates[ game.coordinates.length-2 ];
-
-//         differenceX = previousCoordinates[0] - x;
-//         differenceY = previousCoordinates[1] - y;
-
-//         if( checkDifferent(index) == true){
-
-//             if(
-//                 differenceX <= 1 && differenceX >= -1 && differenceY===0 ||
-//                 differenceY <= 1 && differenceY >= -1 && differenceX === 0
-//             ){
-
-//                 // console.log(game.path)
-//                 drawPath(x, y, index, previousCoordinates);
-
-//                 if(game.path.length == levels[currentLevel].pathLength){
-
-//                     if(currentLevel == levels.length-1){
-//                         game.end = true;
-//                         displayAlert("You have finished the game");
-//                         // console.log("game end");
-//                     }
-//                     else{
-//                         displayAlert("You Win! buddy!");
-//                         game.win=true;
-//                     }
-//                     // displayGameWinScreen();
-                    
-//                     // nextLevel();
-//                 }
-//             }
-//         else{
-//                 game.path.splice(-1,1);
-//                 game.coordinates.splice(-1,1)
-//             }
-            
-//         }
-//         else{
-//             game.path.splice(-1,1);
-//             game.coordinates.splice(-1,1)
-//         }
-        
-//     };
-    
-// };
-
 function drawPath(x, y, index, previousCoordinates){
 
     var el;
