48cdfa05c511b03fb647370ae8ba8509c4049cf1
Steven de Valk <s1129787@student.windesheim.nl>
2020-01-31 17:20:13 +0100

    removed old functions, move nextLevel to functions

--- js/blockPath.js
+++ js/blockPath.js
@@ -256,22 +256,6 @@ function reset(parseIndex){
 
 }
 
-function drawTestPath(){
-
-    game.mouseDown = true;
-
-    checkDrawPath(gridBlocks[6])
-    checkDrawPath(gridBlocks[10])
-    checkDrawPath(gridBlocks[14])
-
-    game.mouseDown = false;
-}
-
-function testReset(){
-    console.log(game.path);
-    reset(10);
-}
-
 function checkIndex(index){
 
     for(var i = 0; i< game.path.length; i++){
@@ -283,101 +267,6 @@ function checkIndex(index){
     return true
 }
 
-function drawRedBlock(){
-
-    var randomIndex = Math.floor(Math.random()*(gridBlocks.length-1))
-    var el;
-
-    if(checkIndex(randomIndex)){
-        el = document.createElement("div");
-        el.className = "grid-block-red";
-        gridBlocks[randomIndex].append(el);
-        game.redBlocks.push(randomIndex);
-        return;
-    }
-    else{
-        drawRedBlock();
-        return;
-    }
-
-}
-
-/*#####################################################\
- *|                                                    #
- *| 3. level class                                     #
- *|                                                    #
-\#####################################################*/
-
-// class level {
-//     constructor(gridHeight, gridWidth, pathStartX, pathStartY, redBlocks){
-//         this.gridHeight = gridHeight;
-//         this.gridWidth = gridWidth;
-//         this.pathStartX = pathStartX;
-//         this.pathStartY = pathStartY
-//         this.redBlocks = redBlocks;
-//         this.pathLength = (this.gridHeight*gridWidth) - redBlocks.length;
-//     }
-
-//     generate(){
-
-//         mainMenu.style.display = "none";
-
-//         for(var i=0; i<this.gridHeight; i++){
-//             var row=document.createElement("div");
-//             row.className = "grid-row";
-//             gameGrid.append(row)
-//             for(var e=0;e<this.gridWidth;e++){
-//                 var el = document.createElement("div");
-//                 el.className="grid-block";
-//                 el.style.height = grid.blockHeight + "px";
-//                 el.style.width = grid.blockWidth + "px";
-//                 el.style.margin = grid.blockMargin + "px";
-//                 row.append(el);
-//             }
-//         }
-
-//         gameGrid.style.width = (grid.blockWidth+grid.blockMargin*2)*this.gridWidth+"px";
-//         gameGrid.style.height = (grid.blockHeight+grid.blockMargin*2)*this.gridHeight+"px";
-    
-//         var pathStart = document.createElement("div");
-//         pathStart.id = "path-start";
-//         pathStart.className = "path";
-//         pathStart.style.height = grid.blockHeight + "px";
-//         pathStart.style.width = grid.blockWidth + "px";
-    
-//         document.getElementsByClassName("grid-row")[this.pathStartY].getElementsByClassName("grid-block")[this.pathStartX].append(pathStart);
-    
-//         game.coordinates.push([this.pathStartX, this.pathStartY, indexInClass(pathStart)])
-//         game.path.push(indexInClass(pathStart.parentElement))
-
-//         pathBlocks = document.getElementsByClassName("path");
-
-//         this.drawRedBlocks();
-
-//         addGridEvents();
-
-//     }
-
-//     drawRedBlocks(){
-//         var el;
-//         if(this.redBlocks!==undefined && this.redBlocks.length!==0 ){
-//             this.redBlocks.forEach(function(item, index){
-//                 el = document.createElement("div");
-//                 el.className = "grid-block-red";
-//                 el.style.height = grid.blockHeight + "px";
-//                 el.style.width = grid.blockWidth + "px";
-//                 gridBlocks[item].append(el);
-//                 game.redBlocks.push(item);
-//             });
-//         }
-//     }
-
-//     clear(){
-//         gameGrid.innerHTML="";
-//     }
-
-// }
-
 function nextLevel(){
     resetGrid();
     currentLevel+=1;
@@ -386,6 +275,14 @@ function nextLevel(){
 
     
 }
+
+/*#####################################################\
+ *|                                                    #
+ *| 3. level class                                     #
+ *|                                                    #
+\#####################################################*/
+
+
 
 /*#####################################################\
  *|                                                    #
